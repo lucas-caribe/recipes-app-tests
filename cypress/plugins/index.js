75cb06f43d1c193a763d39907327d76035d10aa0
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2020-11-25 16:15:09 -0300

    adiciona configuração para o chrome

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,11 @@ module.exports = ( on, config ) => {
             return clipboardy.readSync();
         }
     });
+    on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+            launchOptions.args.push('--disable-dev-shm-usage');
+            return launchOptions;
+        }
+        return launchOptions;
+    });
 };
